a40513d14e9f8657b4b7fa68ac4449f6d45d519c
TA remote <ta-webft-rmt@ironhack.com>
2021-02-09 14:03:44 +0100

    deleted bin folder

--- app.js
+++ app.js
@@ -32,32 +32,3 @@ app.use('/', index);
 require('./error-handling')(app);
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Middleware Setup
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// // Express View engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
